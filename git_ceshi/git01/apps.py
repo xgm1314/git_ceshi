75afa4a003196a1d5cda7f87fc143195274b4a22
xgm01 <153094236@qq.com>
2023-03-14 21:04:19 +0800

    'apps测试'

--- git_ceshi/git01/apps.py
+++ git_ceshi/git01/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class Git01Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'git01'
+    verbose_name = '书籍管理'                       # 更改站点名称
